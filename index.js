e29606274912ee7e8ebd1f03452c7abca79b52ec
Evans <evans.ogonda@student.moringaschool.com>
2022-08-16 20:32:58 +0300

    written all the four functions

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    let numOfBlocksFromScuberHQ;
+    if (location < 43) {
+        numOfBlocksFromScuberHQ = 43- location;
+    }else {
+        numOfBlocksFromScuberHQ = location - 43;
+    }
+
+    return numOfBlocksFromScuberHQ;
+}
+
+
+function distanceFromHqInBlocks(locationInFeet) {
+    let numOfBlocksFromScuberHQInfeet = distanceFromHqInBlocks(locationInFeet);
+    return numOfBlocksFromScuberHQInfeet * 264;
+}
+
+
+function distanceFromHqInBlocksInfeet(start,destination){
+     let numOfBlocksFromScuberHQInfeet;
+     if (start < destination) {
+        numOfBlocksFromScuberHQInfeet = (destination - start) * 264;
+
+     }else{
+        numOfBlocksFromScuberHQInfeet = (start - destination) * 264;
+     }
+     return numOfBlocksFromScuberHQInfeet;
+
+    }
+
+
+
+function calculatesFarePrice(start, destination){
+    let customerDistance = distanceTravelledInFeet(start,destination);
+    let customerFare
+    if (customerDistance < 500){
+        customerFare= customerDistance *0;
+        return customerFare;
+
+
+    }else if (customerDistance < 2500) {
+        let customerDistance2 = customerDistance - 500;
+        customerFare= (customerDistance2 * 2) / 100 ;
+        return customerFare;
+
+    }else if (customerDistance < 2500){
+        let additionalCharge = 25;
+        return additionalCharge;
+
+    }else {
+        return 'cannot travel that far';
+    }
+}
+
+
+
+// console.log(distanceFromHqInBlocks(43));
+//console.log(distanceFromHqInFeet(43));
+//console.log(distanceTravelledInFeet(25,33));
+console.log(calculatesFarePrice(25,33));
